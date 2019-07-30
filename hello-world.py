1b59d1ed8c8c770b2f6ee4b29b45102d9a3d17d7
Aayush <aayush.somani111@gmail.com>
2019-07-30 08:53:15 +0530

    Methods on lists

--- hello-world.py
+++ hello-world.py
@@ -24,3 +24,18 @@ print('  world '.strip())  # Strip leading and trailing whitespace; prints "worl
 print()
 
 #https://docs.python.org/3.5/library/stdtypes.html#string-methods
+
+#Lists
+#A list is the Python equivalent of an array, but is resizeable and can contain elements of different types:
+xs = [3, 1, 2]    # Create a list
+print(xs, xs[2])  # Prints "[3, 1, 2] 2"
+print(xs[-1])     # Negative indices count from the end of the list; prints "2"
+xs[2] = 'foo'     # Lists can contain elements of different types
+print(xs)         # Prints "[3, 1, 'foo']"
+xs.append('bar')  # Add a new element to the end of the list
+print(xs)         # Prints "[3, 1, 'foo', 'bar']"
+x = xs.pop()      # Remove and return the last element of the list
+print(x, xs)      # Prints "bar [3, 1, 'foo']"
+
+#https://docs.python.org/3.5/tutorial/datastructures.html#more-on-lists
+
