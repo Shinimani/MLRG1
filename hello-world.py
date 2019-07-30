f1ce31922fd838664d8317761de2c578e68286aa
Aayush <aayush.somani111@gmail.com>
2019-07-30 08:49:52 +0530

    Methods on string

--- hello-world.py
+++ hello-world.py
@@ -10,3 +10,17 @@ hw = hello + ' ' + world  # String concatenation
 print(hw)  # prints "hello world"
 hw12 = '%s %s %d' % (hello, world, 12)  # sprintf style string formatting
 print(hw12)  # prints "hello world 12"
+
+print()
+s = "hello"
+print(s.capitalize())  # Capitalize a string; prints "Hello"
+print(s.upper())       # Convert a string to uppercase; prints "HELLO"
+print(s.rjust(7))      # Right-justify a string, padding with spaces; prints "  hello"
+print(s.center(7))     # Center a string, padding with spaces; prints " hello "
+print(s.replace('l', '(ell)'))  # Replace all instances of one substring with another;
+                                # prints "he(ell)(ell)o"
+print('  world '.strip())  # Strip leading and trailing whitespace; prints "world"
+
+print()
+
+#https://docs.python.org/3.5/library/stdtypes.html#string-methods
