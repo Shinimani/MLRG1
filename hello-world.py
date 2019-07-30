0acf74d436fa6a8d9ec80ce680c5c60994b5b5d6
Aayush <aayush.somani111@gmail.com>
2019-07-30 09:09:21 +0530

    Methods on slicing

--- hello-world.py
+++ hello-world.py
@@ -36,6 +36,18 @@ xs.append('bar')  # Add a new element to the end of the list
 print(xs)         # Prints "[3, 1, 'foo', 'bar']"
 x = xs.pop()      # Remove and return the last element of the list
 print(x, xs)      # Prints "bar [3, 1, 'foo']"
-
+print()
 #https://docs.python.org/3.5/tutorial/datastructures.html#more-on-lists
 
+#Slicing: In addition to accessing list elements one at a time, Python provides
+# concise syntax to access sublists; this is known as slicing:
+
+nums = list(range(5))     # range is a built-in function that creates a list of integers
+print(nums)               # Prints "[0, 1, 2, 3, 4]"
+print(nums[2:4])          # Get a slice from index 2 to 4 (exclusive); prints "[2, 3]"
+print(nums[2:])           # Get a slice from index 2 to the end; prints "[2, 3, 4]"
+print(nums[:2])           # Get a slice from the start to index 2 (exclusive); prints "[0, 1]"
+print(nums[:])            # Get a slice of the whole list; prints "[0, 1, 2, 3, 4]"
+print(nums[:-1])          # Slice indices can be negative; prints "[0, 1, 2, 3]"
+nums[2:4] = [8, 9]        # Assign a new sublist to a slice
+print(nums)               # Prints "[0, 1, 8, 9, 4]"
